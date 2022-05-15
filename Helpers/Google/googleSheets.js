8edad093381d9935f27d6541660697999dc4d577
SHEHEER ALI <geekboy@facebook.com>
2022-05-15 21:25:02 +0530

    path issue for credentials.json

--- Helpers/Google/googleSheets.js
+++ Helpers/Google/googleSheets.js
@@ -2,7 +2,7 @@ const { google } = require("googleapis");
 const { spanner } = require("googleapis/build/src/apis/spanner");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: "/home/geekboy/Desktop/LibraryApp/backend/credentials.json",
+  keyFile: "credentials.json",
   scopes: "https://www.googleapis.com/auth/spreadsheets",
 });
 const client = auth.getClient();
